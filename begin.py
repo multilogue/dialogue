2223189e9e4d8e6ba8c9d2d10a94e295b5e403d9
Machina Ratiocinatrix <136814290+machina-ratio@users.noreply.github.com>
2023-07-02 09:03:27 -0500

    The context of interaction.

--- begin.py
+++ begin.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 # Python
-"""
-
-"""
 from dotenv import load_dotenv
 load_dotenv()
-
 import openai
+
+"""the properly named environment variables are loaded 
+into the OpenAI library by its' __init__"""
 
 print('ok')
